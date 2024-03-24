a57b4a3ec84267bd701becf4f4f2a4bc137a639b
Juan Daniel <jdaniel1569@gmail.com>
2024-03-23 19:46:38 -0600

    primer commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bienvenido a mi repositorio
+
+
+### Agenda java swing  :tw-1f603:
+
+###### Descripción
+Esta es una aplicación sencilla guarda los datos ingresados en un formulario en arrays
+al darle click al los botones de siguiente o anterior mostrara la información del arrary de acuerdo al indice que se encuentre.
+
+###### Herramientas de desarrollo
+- Java openJDK 17
+- NetBeans 21
+- Swing
+
---
+++ src/Principal.java
@@ -0,0 +1,24 @@
+
+import Vista.AgendaView;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JDANIEL
+ */
+public class Principal {
+    
+    
+    public static void main(String[] args) {
+        
+        AgendaView ventana = new AgendaView();
+        
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        
+    }
+}
